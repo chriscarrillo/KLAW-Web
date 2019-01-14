af1c1a467031dcff7db16d455220db34a63ebf50
Curtis Hohl <curtisphohl@gmail.com>
2019-01-14 14:55:16 -0800

    Updated the database credentials to generic
    Co-Authored-By: chriscarrillo <chriscarrillo@outlook.com>

--- backend/entities/database.py
+++ backend/entities/database.py
@@ -16,7 +16,7 @@ import pymssql
 # The following variables need to change once we have the database up and running
 db_url = "DATABASE_URL_HERE"
 db_name = "KLAWDB"
-db_user = "sa"
+db_user = "DATABASE_USERNAME_HERE"
 db_password = "DATABASE_PASSWORD_HERE"
 engine = create_engine(f'mssql+pymssql://{db_user}:{db_password}@{db_url}/{db_name}')
 Session = sessionmaker(bind=engine)
