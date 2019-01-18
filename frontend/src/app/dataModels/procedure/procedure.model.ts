9c336281a5a24044f4aad0e097aa4f121602ff7d
Curtis Hohl <curtisphohl@gmail.com>
2019-01-18 08:50:51 -0800

    added procedure_name to Procedure

--- frontend/src/app/dataModels/procedure/procedure.model.ts
+++ frontend/src/app/dataModels/procedure/procedure.model.ts
@@ -5,6 +5,7 @@ export class Procedure {
   	public proc_id: Guid,
   	public user_id: Guid,
     public raw_xml: string,
+    public procedure_name: string,
     public _saved_procedure?: string,
   ) { }
 }
