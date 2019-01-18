4df1df82c4b4df2918475d2ac58e7535f3913414
Curtis Hohl <curtisphohl@gmail.com>
2019-01-18 09:01:39 -0800

    added backend code to update procedure

--- backend/entities/procedure.py
+++ backend/entities/procedure.py
@@ -22,12 +22,14 @@ class Procedure(Base):
     proc_id = Column(UUID(), primary_key = True)
     user_id = Column(UUID(), ForeignKey("USER.user_id"), nullable = False)
     raw_xml = Column(Text, nullable = False)
+    procedure_name = Column(String, nullable = False)
     saved_procedure = Column(Text)
 
     def __init__(self, proc_id, user_id, raw_xml, saved_procedure):
         self.proc_id = proc_id
         self.user_id = user_id
         self.raw_xml = raw_xml
+        self.procedure_name = procedure_name
         self.saved_procedure = saved_procedure
 
 class ProcedureSchema(Schema):
@@ -38,4 +40,5 @@ class ProcedureSchema(Schema):
     proc_id = fields.UUID()
     user_id = fields.UUID()
     raw_xml = fields.Str()
+    procedure_name = fields.Str()
     saved_procedure = fields.Str()
