120b683254d0031738e0b54a682a7bf60f6af02e
Curtis Hohl <curtisphohl@gmail.com>
2019-01-22 16:01:16 -0800

    added reorganized components

---
+++ frontend/src/app/components/save-procedure/save-procedure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-save-procedure',
+  templateUrl: './save-procedure.component.html',
+  styleUrls: ['./save-procedure.component.css']
+})
+export class SaveProcedureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
