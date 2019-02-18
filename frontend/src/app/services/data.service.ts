ef37fb36e699c6c944b8aa2c9ece610bbfcd5616
Curtis Hohl <curtisphohl@gmail.com>
2019-02-18 13:36:13 -0800

    Fix data service - not looking for users anymore

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -10,27 +10,10 @@ import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 })
 export class DataService {
 
-  // private isLoggedIn = new BehaviorSubject<boolean>(false); // This is the real data
-  // currentIsLoggedIn = this.isLoggedIn.asObservable(); // This is the Observable that will be used by the components
-
-  private user = new BehaviorSubject<Map<string, string>>(new Map<string, string>()); // The real data
-  currentUser = this.user.asObservable(); // The Observable that will be used by the components
-
   private ROOT_URL = new BehaviorSubject<string>("http://127.0.0.1:5000/api"); //listening to API here
   currentRootUrl = this.ROOT_URL.asObservable();
 
   constructor() { }
-
-  // // Updates isLoggedIn
-  // updateIsLoggedIn(newIsLoggedIn: boolean) {
-  //   this.isLoggedIn.next(newIsLoggedIn);
-  // }
-
-  // Updates user
-  updateUser(newUser: Map<string, string>) {
-    this.user.next(newUser);
-  }
-
   updateRootUrl(newRootUrl: string) {
     this.ROOT_URL.next(newRootUrl);
   }
