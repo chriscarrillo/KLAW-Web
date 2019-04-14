47c52dcc0a6a0b532c4b21e68bf71e01a6fc97bf
Hannah Bernal <hannahbernal02@gmail.com>
2019-04-13 20:00:59 -0700

    Modified wait function

--- frontend/src/app/simulator/simulator.component.ts
+++ frontend/src/app/simulator/simulator.component.ts
@@ -45,6 +45,8 @@ export class SimulatorComponent implements /*OnInit*/ AfterViewInit {
   lowPivot;
   currUpperAngle;
   currLowerAngle;
+  timer;
+  isTimerDone;
 
   constructor(private modelService: ModelService, private eventsService: EventsService) {
     this.render = this.render.bind(this);
@@ -366,23 +368,42 @@ export class SimulatorComponent implements /*OnInit*/ AfterViewInit {
       }
       else {
         // update origDist
+        console.log('Moved claw ' + distanceApart + ' apart!');
         this.origDist = this.leftClaw.position.z - this.rightClaw.position.z - 4;
         prevMethod = animationOrder[0][0];
         animationOrder.shift();
 
       }
+
     }
+
+    //  wait(timeToWait) {
+    //   // add timer here
+    //   setTimeout(function () {
+    //     console.log('Waited ' + timeToWait + ' milliseconds!');
+    //     // alert('Waited ' + timeToWait + ' milliseconds!');
+    //     return;
+    //   }, timeToWait);
+    //   prevMethod = animationOrder[0][0];
+    //   animationOrder.shift();
+    // }
 
   wait(timeToWait) {
       // add timer here
-      setTimeout(function () {
-        console.log('Waited ' + timeToWait + ' milliseconds!');
-        // alert('Waited ' + timeToWait + ' milliseconds!');
-        return;
+    console.log('in WAIT func');
+    console.log('time:', timeToWait);
+    const component: SimulatorComponent = this;
+      this.timer = setTimeout(function () {
+        if (!component.isTimerDone) {
+          component.isTimerDone = true;
+          console.log('Waited ' + timeToWait + ' milliseconds!');
+          prevMethod = animationOrder[0][0];
+          animationOrder.shift();
+          // clearTimeout(component.timer);
+        }
+
       }, timeToWait);
-      prevMethod = animationOrder[0][0];
-      animationOrder.shift();
-    }
+  }
 
   resetModel() {
         this.scene.remove(this.model);
@@ -398,7 +419,6 @@ export class SimulatorComponent implements /*OnInit*/ AfterViewInit {
       // checks to see which animation method is being called
       if (animMethod[0] == 'moveArm') {
         if (prevMethod != null && prevMethod == 'moveArm') {
-          console.log('MODEL TEST');
           // add wait to make transition less jerky
           // setTimeout(function () {
           //   component.resetModel();
@@ -413,11 +433,18 @@ export class SimulatorComponent implements /*OnInit*/ AfterViewInit {
         this.renderer.render(this.scene, this.camera);
       }
       else if (animMethod[0] == 'moveClaw') {
-        this.moveClawFunction((animMethod[1]));
+        this.moveClawFunction(animMethod[1]);
         this.renderer.render(this.scene, this.camera);
       }
       else if (animMethod[0] == 'wait') {
+        this.isTimerDone = false;
+        console.log('wait parameter:', animMethod[1]);
         this.wait(animMethod[1]);
+
+        if (this.isTimerDone) {
+          console.log('clearing timer');
+          clearTimeout(this.timer);
+        }
         this.renderer.render(this.scene, this.camera);
       }
     }
--- static/styles.js
+++ static/styles.js
@@ -598,8 +598,8 @@ if(false) {}
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\klaw\frontend\node_modules\@angular\material\prebuilt-themes\indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
-module.exports = __webpack_require__(/*! C:\klaw\frontend\src\styles.css */"./src/styles.css");
+__webpack_require__(/*! c:\Pulled_KLAW\KLAW-Web\frontend\node_modules\@angular\material\prebuilt-themes\indigo-pink.css */"./node_modules/@angular/material/prebuilt-themes/indigo-pink.css");
+module.exports = __webpack_require__(/*! c:\Pulled_KLAW\KLAW-Web\frontend\src\styles.css */"./src/styles.css");
 
 
 /***/ })
